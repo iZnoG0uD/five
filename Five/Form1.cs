9b9cf21297a3a51dfcf71d24764bfd6a2396df1d
iZnoG0uD <zelesnac@gmail.com>
2021-06-30 13:50:26 +0300

    Добавил выбор сложности

--- Five/Form1.cs
+++ Five/Form1.cs
@@ -5,10 +5,10 @@ namespace Five
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1(int choiceDiff)
         {
             InitializeComponent();
-            LogicGame logic = new LogicGame(panel1, 4);
+            LogicGame logic = new LogicGame(panel1, choiceDiff);
             Size = new Size(panel1.Width + 40, panel1.Height + 65);
         }
     }
